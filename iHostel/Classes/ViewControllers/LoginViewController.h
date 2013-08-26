c103e09d934b33558a18c6edac689a6421aa29ae
friklergit <longdang@Macs-Mac-mini.local>
2013-08-26 18:10:58 +0700

    First Commit
    First Commit

---
+++ iHostel/Classes/ViewControllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  iHostel
+//
+//  Created by long.dang on 8/26/13.
+//  Copyright (c) 2013 DHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ iHostel/Classes/ViewControllers/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  iHostel
+//
+//  Created by long.dang on 8/26/13.
+//  Copyright (c) 2013 DHL. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
